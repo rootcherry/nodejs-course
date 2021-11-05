ea30bfa8879daf962c8749a4bcb9c263e37bb598
Chris N <hi.chrisn@gmail.com>
2021-11-04 21:41:15 -0300

    HTTP request challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,21 @@
 const config = require('../config');
 const request = require('request');
 
-const url = `http://api.weatherstack.com/current?access_key=${config.key}&query=37.8267,-122.4233`;
+const url = `http://api.weatherstack.com/current?access_key=${config.wsKey}&query=37.8267,-122.4233`;
 
 request({url: url, json: true }, (error, response) => {
   // console.log(response.body.current);
   const ob = response.body.current;
   console.log(`${ob.weather_descriptions[0]}. It is currently ${ob.temperature} degress out. It feels like ${ob.feelslike} degress out.`);
+});
+
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${config.mbKey}`;
+
+request({
+  url: geocodeURL,
+  json: true
+}, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+  console.log(latitude, longitude);
 });

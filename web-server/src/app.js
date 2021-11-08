a76302c5f516f245de03122cb7f2257b177d9e6b
Chris N <hi.chrisn@gmail.com>
2021-11-08 15:53:52 -0300

    dynamic pages with templating - handlebars.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,9 +7,30 @@ const app = express();
 // console.log(path.join(__dirname, '../public'));
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
 // routes
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'John'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'John Doe'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'Yeehaw!'
+  });
+});
+
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'It is snowing',

7cdc11d518eb5d29687ec44e5591208c24d3698b
Chris N <hi.chrisn@gmail.com>
2021-11-08 13:46:56 -0300

    serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,21 +3,30 @@ const app = express();
 
 // routes
 app.get('/', (req, res) => {
-  res.send('Hello, express!');
+  res.send('<h1>Weather</h1>');
 })
 
 app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.send([{
+    name: 'John',
+    age: 21
+  }, {
+    name: 'Jane',
+    age: 23
+  }]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.send('<h1>About page</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('View weather');
+  res.send({
+    forecast: 'It is snowing',
+    location: 'Philadelphia, PA, USA'
+  });
 });
-
+ 
 app.listen(3000, () => {
   console.log('Server is running on port 3000.');
 });

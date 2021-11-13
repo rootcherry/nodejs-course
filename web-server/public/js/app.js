f14ef68e29c2228f3eb9c1dfb985e07029958d9e
Chris N <hi.chrisn@gmail.com>
2021-11-13 11:17:04 -0300

    user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -10,19 +10,24 @@ console.log('Client side javascript file is loaded!');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const msgOne = document.querySelector('#msg-1');
+const msgTwo = document.querySelector('#msg-2');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const location = search.value;
 
+  msgOne.textContent = 'Loading...';
+  msgTwo.textContent = '';
+
   fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error);
+        msgOne.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forecast;
       }
     });
   });
